1a3910af1e54a0fe1e0678bfb15467673c955a40
rabihak <rabihaksamawati@gmail.com>
2024-08-03 20:11:32 +0300

    openRouter api integration upgrade chat model to gpt 4o-mini

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import '@fontsource/inter';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const inter = Inter({ subsets: ["latin"] });
